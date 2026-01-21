27d2c9d1cd5cd6e6e24c6e0228bb5a40bb3b4439
neves369 <douglasbrian369@hotmail.com>
2026-01-21 15:16:59 -0300

    novas áreas

--- src/componentes/Mapa.jsx
+++ src/componentes/Mapa.jsx
@@ -1,42 +1,91 @@
-
 import React, { useState } from "react";
 import mapaImage from "../assets/mapa.jpg";
-import "./Mapa.css"; 
+import "./Mapa.css";
 
 export default function ImagemClicavel() {
   const [popupInfo, setPopupInfo] = useState(null);
 
   const hotspots = [
-    { id: "area1", top: "23%", left: "56%", width: "15%", height: "35%", transform: "rotate(146deg)",
-         info:(
-            <div>
-                <h2>Informações da Área 1</h2>
-            </div>
-         )  },
-    { id: "area2", top: "40%", left: "41%", width: "6%", height: "35%", 
-        info:(
-            <div>
-                <h2>Detalhes da Área 2</h2>
-            </div>
-        ) },
-    { id: "area3", top: "1%", left: "11%", width: "32%", height: "33%",
-         info: (
-            <div>
-                <h2>Detalhes da Área 3</h2>
-            </div>
-         ) },
-    { id: "area4", top: "43%", left: "11%", width: "30%", height: "23%", 
-        info: (
-            <div>
-                <h2>Detalhes da Área 4</h2>
-            </div>
-        ) },
-    { id: "area5", top: "75%", left: "0%", width: "63%", height: "25%",
-         info: (
-            <div>
-                <h2>Informações da Área 5</h2>
-            </div>
-         ) },
+    {
+      id: "area1",
+      top: "23%",
+      left: "56%",
+      width: "14%",
+      height: "35%",
+      transform: "rotate(146deg)",
+      info: {
+        titulo: "Informações da Área 1",
+        descricao: "Detalhes adicionais sobre a Área 1",
+      },
+    },
+    {
+      id: "area2",
+      top: "40%",
+      left: "41%",
+      width: "4%",
+      height: "35%",
+      info: {
+        titulo: "Informações da Área 2",
+        descricao: "Detalhes adicionais sobre a Área 2",
+      },
+    },
+    {
+      id: "area3",
+      top: "0%",
+      left: "21.5%",
+      width: "21%",
+      height: "34%",
+      info: {
+        titulo: "Informações da Área 3",
+        descricao: "Detalhes adicionais sobre a Área 3",
+      },
+    },
+    {
+      id: "area4",
+      top: "44%",
+      left: "11.5%",
+      width: "5%",
+      height: "22%",
+      info: {
+        titulo: "Informações da Área 4",
+        descricao: "Detalhes adicionais sobre a Área 4",
+      },
+    },
+    {
+      id: "area5",
+      top: "75%",
+      left: "0%",
+      width: "63%",
+      height: "25%",
+      info: {
+        titulo: "Informações da Área 5",
+        descricao: "Detalhes adicionais sobre a Área 5",
+      },
+    },
+    {
+      id: "piquenique",
+      top: "0%",
+      left: "11.5%",
+      width: "10%",
+      height: "34%",
+      backgroundColor: "rgba(0, 192, 250, 0.3)",
+      info: {
+        titulo: "Área de Piquenique",
+        descricao: "Detalhes adicionais sobre a Área de Piquenique",
+      },
+    },
+    {
+      id: "canteiro",
+      top: "44%",
+      left: "16.5%",
+      width: "24.5%",
+      height: "22%",
+      backgroundColor: "rgba(0, 192, 250, 0.3)",
+      info: {
+        titulo: "Informações da Área 4",
+        descricao: "Detalhes adicionais sobre a Área 4",
+      },
+    },
   ];
 
   const openPopup = (info) => {
@@ -48,34 +97,43 @@ export default function ImagemClicavel() {
   };
 
   return (
-    
     <div className="todo_cont">
       <div className="image-container">
-      
-      <img src={mapaImage} alt="Mapa" />
+        <img src={mapaImage} alt="Mapa" />
 
-      {hotspots.map(({ id, top, left, width, height, info }) => (
-        <button
-          key={id}
-          className="hotspot"
-          style={{ top, left, width, height, transform: hotspots.find(h => h.id === id).transform || 'none' }}
-          onClick={() => openPopup(info)}
-        />
-      ))}
+        {hotspots.map(({ id, top, left, width, height, info }) => (
+          <button
+            key={id}
+            className="hotspot"
+            style={{
+              top,
+              left,
+              width,
+              height,
+              backgroundColor:
+                hotspots.find((h) => h.id === id).backgroundColor ||
+                "rgba(255, 0, 0, 0.3)",
+              transform: hotspots.find((h) => h.id === id).transform || "none",
+            }}
+            onClick={() => openPopup(info)}
+          />
+        ))}
 
-      {popupInfo && (
-        <div className="popup">
-          <div className="popup-content">
-            <button className="close-btn" onClick={closePopup}>
-              &times;
-            </button>
-            <p>{popupInfo}</p>
+        {popupInfo && (
+          <div className="popup">
+            <div className="popup-content">
+              <div className="popup-title">
+                <span className="close" onClick={closePopup}>
+                  &times;
+                </span>
+                <p className="retro-text">{popupInfo.titulo}</p>
+              </div>
+              <p className="popup-info">{popupInfo.descricao}</p>
+            </div>
+            <div className="popup-overlay" onClick={closePopup}></div>
           </div>
-          <div className="popup-overlay" onClick={closePopup}></div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
-    </div>
-    
   );
 }
