12f5ab718a681d6ff7abefb36d5417ea0e791c85
neves369 <douglasbrian369@hotmail.com>
2026-01-23 14:31:52 -0300

    teste mobile

--- src/pages/Mobile.jsx
+++ src/pages/Mobile.jsx
@@ -1,51 +1,46 @@
-import Mapa from '../componentes/Mapa'
-import React, { useState, useEffect } from 'react'
-import '../../styles/mobile.css'
-
+import Mapa from "../componentes/Mapa";
+import React, { useState, useEffect } from "react";
+import "../../styles/mobile.css";
 
 function Mobile() {
+  // const [error, setError] = useState(null);
+  // const [position, setPosition] = useState(null);
 
-  const [error, setError] = useState(null);
-  const [position, setPosition] = useState(null);
+  // useEffect(() => {
+  //   // Verifica se o navegador suporta geolocalização
+  //   if (!navigator.geolocation) {
+  //     // eslint-disable-next-line react-hooks/set-state-in-effect
+  //     setError("Geolocalização não é suportada pelo seu navegador.");
+  //     return;
+  //   }
 
+  //   // Pede a permissão e pega a posição
+  //   navigator.geolocation.getCurrentPosition(
+  //     (pos) => {
+  //       setPosition({
+  //         lat: pos.coords.latitude,
+  //         lng: pos.coords.longitude,
+  //       });
+  //     },
+  //     (err) => {
+  //       setError("Não foi possível obter sua localização.");
+  //       console.error(err);
+  //     },
+  //     { enableHighAccuracy: true },
+  //   );
+  // }, []);
 
-  useEffect(() => {
-    // Verifica se o navegador suporta geolocalização
-    if (!navigator.geolocation) {
-      // eslint-disable-next-line react-hooks/set-state-in-effect
-      setError('Geolocalização não é suportada pelo seu navegador.');
-      return;
-    }
-
-    // Pede a permissão e pega a posição
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        setPosition({
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        });
-      },
-      (err) => {
-        setError('Não foi possível obter sua localização.');
-        console.error(err);
-      },
-      { enableHighAccuracy: true } 
-    );
-  }, []);
-
-  if (error) return <p>{error}</p>;
-  if (!position) return <p>Obtendo localização...</p>;
+  // if (error) return <p>{error}</p>;
+  // if (!position) return <p>Obtendo localização...</p>;
 
   return (
     <>
-      <div className='card'>
-          
-          <Mapa />
-          <p>Lat: {position.lat} | Lng: {position.lng}</p>
+      <div className="card">
+        <Mapa />
+        <p>{/* Lat: {position.lat} | Lng: {position.lng} */}</p>
       </div>
-    
     </>
-  )
+  );
 }
 
-export default Mobile
+export default Mobile;
