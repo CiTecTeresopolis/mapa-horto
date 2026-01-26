c3adc77478d62ff9ccd1d24fd6459f18ddee489b
Secretaria de Ciência e Tecnologia <cienciaetecnologia@teresopolis.rj.gov.br>
2026-01-26 14:16:25 -0300

    retorno do mobile

--- src/pages/Mobile.jsx
+++ src/pages/Mobile.jsx
@@ -3,41 +3,41 @@ import React, { useState, useEffect } from "react";
 import "../../styles/mobile.css";
 
 function Mobile() {
-  // const [error, setError] = useState(null);
-  // const [position, setPosition] = useState(null);
+  const [error, setError] = useState(null);
+  const [position, setPosition] = useState(null);
 
-  // useEffect(() => {
-  //   // Verifica se o navegador suporta geolocalização
-  //   if (!navigator.geolocation) {
-  //     // eslint-disable-next-line react-hooks/set-state-in-effect
-  //     setError("Geolocalização não é suportada pelo seu navegador.");
-  //     return;
-  //   }
+  useEffect(() => {
+    // Verifica se o navegador suporta geolocalização
+    if (!navigator.geolocation) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setError("Geolocalização não é suportada pelo seu navegador.");
+      return;
+    }
 
-  //   // Pede a permissão e pega a posição
-  //   navigator.geolocation.getCurrentPosition(
-  //     (pos) => {
-  //       setPosition({
-  //         lat: pos.coords.latitude,
-  //         lng: pos.coords.longitude,
-  //       });
-  //     },
-  //     (err) => {
-  //       setError("Não foi possível obter sua localização.");
-  //       console.error(err);
-  //     },
-  //     { enableHighAccuracy: true },
-  //   );
-  // }, []);
+    // Pede a permissão e pega a posição
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+      },
+      (err) => {
+        setError("Não foi possível obter sua localização.");
+        console.error(err);
+      },
+      { enableHighAccuracy: true },
+    );
+  }, []);
 
-  // if (error) return <p>{error}</p>;
-  // if (!position) return <p>Obtendo localização...</p>;
+  if (error) return <p>{error}</p>;
+  if (!position) return <p>Obtendo localização...</p>;
 
   return (
     <>
       <div className="card">
         <Mapa />
-        <p>{/* Lat: {position.lat} | Lng: {position.lng} */}</p>
+        <p>{position.lat} | Lng: {position.lng}</p>
       </div>
     </>
   );
